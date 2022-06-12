72d5711d0ad759bb8a6d31ece63716a5af44aeaa
zlatimirCS <zlatimir.ramac@qsense.ch>
2022-06-12 13:28:07 +0200

    initial z commit

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <h2>reminder project setup z</h2>;
 }
 
 export default App;
