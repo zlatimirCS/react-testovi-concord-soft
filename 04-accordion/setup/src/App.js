ede466e0c4d6719fcdc9f3ce3ebcf52f95e2fd4c
zlatimirCS <zlatimir.ramac@qsense.ch>
2022-06-12 14:17:12 +0200

    accordion install

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return <h2 className="test">accordion project setup</h2>;
 }
 
 export default App;
