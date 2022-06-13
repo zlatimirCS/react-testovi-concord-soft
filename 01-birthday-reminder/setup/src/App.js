4902e03743ad468d12115cdb59f64868ac219fdf
Radovan Buljic <buljic.radovan@gmail.com>
2022-06-13 18:36:05 +0200

    inicijalni u brancu radovan

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2 className="test">Birthday reminder project setup radovan test23asd</h2>;
+  return (
+    <h2 className="test">
+      Birthday reminder project setup radovan test23asd,asdf
+    </h2>
+  );
 }
 
 export default App;
