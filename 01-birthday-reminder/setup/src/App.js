430a77d94db6aab80bdf7715e9c06dba251188da
Radovan Buljic <buljic.radovan@gmail.com>
2022-06-13 18:34:04 +0200

    inicijalni

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2 className="test">Birthday reminder project setup </h2>;
+  return <h2 className="test">Birthday reminder project setup, asdf </h2>;
 }
 
 export default App;
