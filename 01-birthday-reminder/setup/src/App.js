c7e6778f3155092e43906bb855181d84ca9cf68f
zlatimirCS <zlatimir.ramac@qsense.ch>
2022-06-13 17:52:56 +0200

    another change

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2 className="test">Birthday reminder project setup radovan</h2>;
+  return <h2 className="test">Birthday reminder project setup radovan test</h2>;
 }
 
 export default App;
