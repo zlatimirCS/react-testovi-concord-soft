7b782b906fd01943af1294610eccfd69744b0476
sanja-vukelic <sanja.vukelic6@gmail.com>
2022-06-14 09:22:14 +0200

    dodatne promene

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
-  return <h2 className="test">Birthday reminder project setup, asdf </h2>;
+  return <h2 className="test">Birthday reminder project setup, asdf test</h2>;
 }
 
 export default App;
