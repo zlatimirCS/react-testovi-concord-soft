1fde9cf52ceb7983450b087cde2059450c7339f6
srdjan-react <srdjan@tantumcreations.com>
2022-06-20 16:06:42 +0200

    changed color and text

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
+
 function App() {
-  return <h2 className="test">Birthday reminder project setup, asdf </h2>;
+    return <h2 className = "test" > Birthday reminder project setup, asdf Srdjan < /h2>;
 }
 
 export default App;
