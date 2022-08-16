91ac9798e5121cd3d1b1aa91fa9f60a973669059
basicpalkovic <palkovic44@gmail.com>
2022-08-16 09:30:32 +0200

    izmena app file

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
-  return <h2 className="test">Birthday reminder project setup, asdf </h2>;
+  return <h2 className="test">Birthday reminder project setup, luka </h2>;
 }
 
 export default App;
