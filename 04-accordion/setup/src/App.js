a2c7b59906035f5e0b220bb54df087de64f7de99
Radovan Buljic <buljic.radovan@gmail.com>
2022-09-11 15:12:09 +0200

    o4 first commit

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,26 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React from "react";
+import data from "./data";
+import styles from "./app.module.scss";
+import Question from "./Question";
 function App() {
-  return <h2 className="test">accordion project setup</h2>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.main_wrapper}>
+        <div className={styles.main_container}>
+          <div className={styles.headline}>
+            <h3>questions and answers about login</h3>
+          </div>
+          <div className={styles.question_container}>
+            {/* map sequence start */}
+            {data.map((quetion) => {
+              return <Question key={quetion.id} question={quetion} />;
+            })}
+            {/* map sequence end */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,37 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import styles from "./question.module.scss";
+const Question = (props) => {
+  const [visibleQuestion, setVisibleQuestion] = useState(false);
+
+  function visibleQUbutton() {
+    if (visibleQuestion) {
+      setVisibleQuestion(false);
+    } else {
+      setVisibleQuestion(true);
+    }
+  }
+  return (
+    <div className={styles.question_item_container}>
+      <div className={styles.item_headline_container}>
+        <div className={styles.item_headline}>
+          <h4>{props.question.title}</h4>
+        </div>
+        <div className={styles.item_button} onClick={visibleQUbutton}>
+          {visibleQuestion ? (
+            <AiOutlineMinus className={styles.btnbtn} />
+          ) : (
+            <AiOutlinePlus className={styles.btnbtn} />
+          )}
+        </div>
+      </div>
+      {visibleQuestion ? (
+        <div className={styles.question_item_answer}>
+          <p>{props.question.info}</p>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default Question;
