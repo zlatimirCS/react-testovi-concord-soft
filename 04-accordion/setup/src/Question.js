6ce03cb19e5d07c1c9289ec27d7f2be3997bbd21
Radovan Buljic <buljic.radovan@gmail.com>
2022-09-11 17:46:05 +0200

    zavrsen zadatak

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -3,6 +3,42 @@ import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import styles from "./question.module.scss";
 const Question = (props) => {
   const [visibleQuestion, setVisibleQuestion] = useState(false);
+  // outline plus replacement
+  const AiOutlinePlusReplacement = (props) => {
+    const {
+      props: {
+        attr: { pId, ...rest },
+        children: [defs, ...paths],
+      },
+    } = AiOutlinePlus();
+
+    const SAFE_PATHS = paths.map(
+      ({ type, props: { pId: pathId, ...pathProps } }) =>
+        React.createElement(type, pathProps)
+    );
+    const SAFE_CHILDREN = React.Children.toArray([defs, ...SAFE_PATHS]);
+    const SAFE_PROPS = { attr: rest, ...props };
+
+    return React.cloneElement(AiOutlinePlus(), SAFE_PROPS, SAFE_CHILDREN);
+  };
+
+  // outline minus desplacement
+  const AiOutlineMinusReplacement = (props) => {
+    const {
+      props: {
+        attr: { pId, ...rest },
+        children: [defs, ...paths],
+      },
+    } = AiOutlineMinus();
+
+    const SAFE_PATHS = paths.map(
+      ({ type, props: { pId: pathId, ...pathProps } }) =>
+        React.createElement(type, pathProps)
+    );
+    const SAFE_CHILDREN = React.Children.toArray([defs, ...SAFE_PATHS]);
+    const SAFE_PROPS = { attr: rest, ...props };
+    return React.cloneElement(AiOutlineMinus(), SAFE_PROPS, SAFE_CHILDREN);
+  };
 
   function visibleQUbutton() {
     if (visibleQuestion) {
@@ -11,6 +47,7 @@ const Question = (props) => {
       setVisibleQuestion(true);
     }
   }
+
   return (
     <div className={styles.question_item_container}>
       <div className={styles.item_headline_container}>
@@ -19,9 +56,11 @@ const Question = (props) => {
         </div>
         <div className={styles.item_button} onClick={visibleQUbutton}>
           {visibleQuestion ? (
-            <AiOutlineMinus className={styles.btnbtn} />
+            // <AiOutlineMinus className={styles.btnbtn} />
+            <AiOutlineMinusReplacement className={styles.btnbtn} />
           ) : (
-            <AiOutlinePlus className={styles.btnbtn} />
+            <AiOutlinePlusReplacement className={styles.btnbtn} />
+            // <AiOutlinePlus className={styles.btnbtn} />
           )}
         </div>
       </div>
