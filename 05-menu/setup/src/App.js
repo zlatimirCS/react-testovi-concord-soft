3fdc3376ee169cd537551b0665c5cdaf202a9e52
Radovan Buljic <buljic.radovan@gmail.com>
2022-09-12 15:38:48 +0200

    up-to-data

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+import styles from "./app.module.scss";
 
 function App() {
-  return <h2 className="test">menu project setup</h2>;
+  return (
+    <main className={styles.main_wrapper}>
+      <div className={styles.main_container}>
+        <div className={styles.main_headline_container}>
+          <div className={styles.headline}>
+            <h2>our menu</h2>
+          </div>
+          <div className={styles.underline}></div>
+        </div>
+        <Categories />
+      </div>
+    </main>
+  );
 }
 
 export default App;
