9c78565ccf401abe6143f86fdfb7e01dd68cce4f
otasch@yahoo.com <otasch@yahoo.com>
2022-11-10 23:39:27 +0100

    Initial project setup with basic structure

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
-  return <h2 className="test">Birthday reminder project setup, asdf </h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <div className="app">
+      <h3>{people.length} birthdays today </h3>
+      <List people={people} />
+      <button onClick={() => setPeople([])}>Clear All</button>
+    </div>
+  );
 }
 
 export default App;
