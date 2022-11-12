0f92bc640b0ef94fc886a336bdb676b59fc8aaf1
otasch@yahoo.com <otasch@yahoo.com>
2022-11-12 20:52:33 +0100

    upload project

--- 01-birthday-reminder/setup/README.md
+++ 01-birthday-reminder/setup/README.md
@@ -1,21 +1,3 @@
-## Uputstvo
+# Birthday Reminder React App
 
-klonirati sve projekte
-git clone https://github.com/zlatimirCS/react-testovi-concord-soft.git
-
-navigirati u folder /01-birthday-reminder/setup
-
-npm install
-
-kreirati svoj branch
-git checkout -b {naziv-brancha}
-
-git push
-
-github username: zlatimir_rk@yahoo.com
-
-Ocekivani rezultat
-https://react-projects-1-birthday-reminder.netlify.app/
-
-Rezultat:
-https://birthday-reminder2022.netlify.app/
+Aleksa Otasevic
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = ({ people }) => {
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <div className="list">
+      <ul>
+        {people.map((person) => {
+          return (
+            <li key={person.id}>
+              <div>
+                <img src={person.image} alt={person.name} />
+              </div>
+              <div>
+                <h4>{person.name}</h4>
+                <p>{person.age} years</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
