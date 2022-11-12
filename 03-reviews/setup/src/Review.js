21880a8f4fa902804426a42791f4111e846d1413
otasch@yahoo.com <otasch@yahoo.com>
2022-11-12 21:11:52 +0100

    Set basic UI and functionality

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,49 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [id, setId] = useState(0);
+  const numOfPersons = people.length - 1;
+  const previousHandler = () => {
+    setId((prev) => {
+      if (id === 0) return numOfPersons;
+      return prev - 1;
+    });
+  };
+  const nextHandler = () => {
+    setId((prev) => {
+      if (id === numOfPersons) return 0;
+      return prev + 1;
+    });
+  };
+  const surpriseHandler = () => {
+    const randomId = Math.floor(Math.random() * 4);
+    setId(randomId);
+  };
+  return (
+    <article className="card">
+      <div className="person">
+        <div className="img-container">
+          <img src={people[id].image} alt="" />
+        </div>
+        <p> {people[id].name}</p>
+        <p> {people[id].job}</p>
+        <p> {people[id].text}</p>
+      </div>
+      <div className="controls">
+        <div>
+          <button onClick={previousHandler}>
+            <FaChevronLeft />
+          </button>
+          <button onClick={nextHandler}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button onClick={surpriseHandler}>Surprise me</button>
+      </div>
+    </article>
+  );
 };
 
 export default Review;
