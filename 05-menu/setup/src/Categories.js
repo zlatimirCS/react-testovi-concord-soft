89c0a599a30a2dbc4d581e54faf8b6f9ea529770
otasch@yahoo.com <otasch@yahoo.com>
2022-11-14 00:25:23 +0100

    Add styles

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -3,40 +3,30 @@ import React from "react";
 const Categories = ({ setCategory }) => {
   return (
     <div className="filter">
-      <ul>
-        <li>
-          <button
-            className="all"
-            onClick={(e) => setCategory(e.target.className)}
-          >
-            all
-          </button>
-        </li>
-        <li>
-          <button
-            className="breakfast"
-            onClick={(e) => setCategory(e.target.className)}
-          >
-            breakfast
-          </button>
-        </li>
-        <li>
-          <button
-            className="lunch"
-            onClick={(e) => setCategory(e.target.className)}
-          >
-            lunch
-          </button>
-        </li>
-        <li>
-          <button
-            className="shakes"
-            onClick={(e) => setCategory(e.target.className)}
-          >
-            shakes
-          </button>
-        </li>
-      </ul>
+      <button className="all" onClick={(e) => setCategory(e.target.className)}>
+        all
+      </button>
+
+      <button
+        className="breakfast"
+        onClick={(e) => setCategory(e.target.className)}
+      >
+        breakfast
+      </button>
+
+      <button
+        className="lunch"
+        onClick={(e) => setCategory(e.target.className)}
+      >
+        lunch
+      </button>
+
+      <button
+        className="shakes"
+        onClick={(e) => setCategory(e.target.className)}
+      >
+        shakes
+      </button>
     </div>
   );
 };
--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -3,11 +3,15 @@ const Menu = ({ items }) => {
     <main className="menu">
       {items.map((item) => {
         return (
-          <article key={item.id}>
+          <article key={item.id} className="meal">
             <img src={item.img} alt={item.title} />
-            <h4> {item.title} </h4>
-            <p> {item.price} </p>
-            <p> {item.desc} </p>
+            <div className="text">
+              <div className="info">
+                <h4> {item.title} </h4>
+                <h4 className="price"> ${Math.ceil(item.price)} </h4>
+              </div>
+              <p className="desc"> {item.desc} </p>
+            </div>
           </article>
         );
       })}
