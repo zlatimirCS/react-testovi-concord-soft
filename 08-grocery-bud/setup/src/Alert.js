e45a0cd0523dac41ac04ef92f28fe0b9f58892f8
otasch@yahoo.com <otasch@yahoo.com>
2022-11-17 13:38:02 +0100

    Init project

--- 08-grocery-bud/setup/src/Alert.js
+++ 08-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import { useEffect } from "react";
 
 const Alert = () => {
-  return <h2>alert component</h2>
-}
+  return <h2>alert component</h2>;
+};
 
-export default Alert
+export default Alert;
