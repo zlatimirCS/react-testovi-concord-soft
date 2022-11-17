97696d76caeec61cb1e2a64e8243ea860eb07a4b
otasch@yahoo.com <otasch@yahoo.com>
2022-11-17 01:37:52 +0100

    Style Error page

--- 07-cocktails/setup/src/pages/Error.js
+++ 07-cocktails/setup/src/pages/Error.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h2>Oops!</h2>
-      <Link to="/">back home</Link>
+    <div className="error">
+      <h2 className="main-title">Oops! It's a dead end</h2>
+      <Link className="btn btn-primary" to="/">
+        back home
+      </Link>
     </div>
   );
 };
