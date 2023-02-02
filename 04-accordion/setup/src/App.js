7be5c66016689dfc57713dffc2397f78b5ddb972
Milan Romic <milan.romic@yahoo.com>
2023-02-02 09:55:48 +0100

    04 Finished

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2 className="test">accordion project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <div className="qwrap">
+          <div className="title">
+            <h2>Questions And Answers About Login</h2>
+          </div>
+          <div className="questions">
+            {data.map(q => 
+              <SingleQuestion key={q.id} question={q} />
+            )}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = (props) => {
+  const [accStatus, setAccStatus] = useState(false);
+
+  const toggleAccordion = () => {
+    setAccStatus(prevStatus => !prevStatus)
+  }
+
+  return (
+    <div className='question-box'>
+      <button className='accordion-button' onClick={toggleAccordion}>
+        {!accStatus && <AiOutlinePlus />}
+        {accStatus && <AiOutlineMinus />}
+      </button>
+      <h4>{props.question.title}</h4>
+      <div className={`accordion-text ${accStatus ? "active" : ""}`}>
+        {props.question.info}
+      </div>
+    </div>
+  )
 };
 
 export default Question;
