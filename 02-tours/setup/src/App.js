80e5b67ed3ddd3a23d05e41dd592ed7748c8c090
Technologic <musicki.aleksandar998@gmail.com>
2023-03-25 21:07:26 +0100

    Tours Completed

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,60 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
 
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
-  return <h2 className="test">Tours Project Setup</h2>
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id != id);
+    setTours(newTours);
+  };
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <div className='title-underline'></div>
+          <button onClick={() => fetchTours()} className='btn'>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,51 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+  let [textButton, setTextButton] = useState('Read more');
+
+  const toggleReadMore = () => {
+    if (readMore) {
+      setReadMore(!readMore);
+      textButton = 'Read more';
+      setTextButton(textButton);
+    } else {
+      setReadMore(!readMore);
+      textButton = 'Read less';
+      setTextButton(textButton);
+    }
+  };
+
+  return (
+    <article className='tour'>
+      <section>
+        <img src={image} alt={name} className='tour__image' />
+        <span className='tour__price'>${price}</span>
+      </section>
+      <section className='tour__info'>
+        <h5>{name}</h5>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}... `}
+          <button
+            type='button'
+            className='tour__info-btn'
+            onClick={() => toggleReadMore()}
+          >
+            {textButton}
+          </button>
+        </p>
+        <section className='center'>
+          <button
+            type='button'
+            className='btn btn-block delete-btn'
+            onClick={() => removeTour(id)}
+          >
+            Not Interested
+          </button>
+        </section>
+      </section>
+    </article>
+  );
 };
 
 export default Tour;
