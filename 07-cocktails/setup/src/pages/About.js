3d39043800a796da1c1831b46a420ffa6cbdd608
Technologic <musicki.aleksandar998@gmail.com>
2023-04-04 18:45:17 +0200

    Cocktails Finished

--- 07-cocktails/setup/src/pages/About.js
+++ 07-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt id
+        distinctio excepturi soluta quia omnis odit aliquid quasi nulla dolor,
+        fugit molestiae corrupti quas voluptate illum exercitationem impedit
+        velit tenetur.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
