e3dba8855cf5a5b6c5842d110230f39d75ba5447
Strahinja Polovina <strahinja.polovina@icloud.com>
2023-07-07 09:45:25 +0200

    Uradjen zadatak review

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Review from './Review';
+import reviews from './data';
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
+import "./App.css";
+
 function App() {
-  return <h2 className="test">reviews project setup</h2>;
+  const [index, setIndex] = useState(0);
+
+  const { id, name, job, image, text } = reviews[index];
+
+  const nextReview = () => {
+    setIndex((index + 1) % reviews.length);
+  };
+
+  const prevReview = () => {
+    setIndex((index + reviews.length - 1) % reviews.length);
+  };
+
+  const randomReview = () => {
+    const randomIndex = Math.floor(Math.random() * reviews.length);
+    setIndex(randomIndex);
+  };
+
+  return (
+    <div>
+      <div className='title'>
+        <h1>Our Reviews</h1>
+        <div className='line'></div>
+      </div>
+      <div className='content'>
+      <Review key={id} review={{ name, job, image, text }} />
+
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevReview}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextReview}>
+          <FaChevronRight />
+        </button>
+      </div>
+
+      <button className="surprise" onClick={randomReview}>
+        Surprise Me
+      </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
-import people from './data';
+import React from 'react';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import "./Review.css";
 
-const Review = () => {
-  return <h2>review component</h2>;
-};
+function Review(props) {
+  const { name, job, image, text } = props.review;
+
+  return (
+    <div className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+    </div>
+  );
+}
 
 export default Review;
