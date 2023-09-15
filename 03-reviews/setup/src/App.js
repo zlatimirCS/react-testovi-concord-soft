1ef9781411119db69cda8e03d686e3580845b483
ninazarkovic <ninaa.zarkovic@gmail.com>
2023-09-15 09:25:33 +0200

    Added reviews task

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Review from './Review';
+import people from './data.js';
+import './styles.css';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+//ovde useState koristimo za indexe --> tako cemo da omoguccimo prikaz samo jednog po jednog
+//pomocu funkcije setCountIndex menjacemo indexe dinamicki kako podesimo funkcionalnost
+//da bi pokupili podatke i prikazali neki treba nam naziv niza i index
+//destruktuiracemo podatke
 function App() {
-  return <h2 className="test">reviews project setup</h2>;
+  const [index, setIndex] = useState(0);
+  const person = people[index];
+
+  const hendleNext = () => {
+    console.log('u NEXT');
+    if (index === people.length - 1) {
+      return setIndex(0);
+    }
+    return setIndex(index + 1);
+  };
+
+  const hendlePrev = () => {
+    console.log('u prev');
+    if (index === 0) {
+      return setIndex(people.length - 1);
+    }
+    return setIndex(index - 1);
+  };
+
+  const handleRandom = () => {
+    let random = Math.floor(Math.random() * people.length);
+
+    if (random === index) {
+      random = index + 1;
+    }
+    if (random > people.length - 1) {
+      return setIndex(0);
+    }
+    return setIndex(random);
+  };
+  return (
+    <main>
+      <section>
+        <div className='title margin-buttom'>
+          <h1>Our Reviews</h1>
+          <span className='decorative'></span>
+        </div>
+        <Review
+          person={person}
+          hendleNext={hendleNext}
+          hendlePrev={hendlePrev}
+          handleRandom={handleRandom}
+        ></Review>
+      </section>
+    </main>
+  );
 }
 
 export default App;
+//Sada, kada trenutni indeks (index) postane jednak poslednjem indeksu (people.length - 1), to znači da ste trenutno prikazali poslednjeg korisnika u nizu.
+//Kada kliknete na "Sledeće" nakon što ste već na poslednjem korisniku, želite se vratiti na prvi korisnik u nizu, što je indeks 0. Zato se koristi index === people.length - 1 kao uslov, da biste znali kada se treba vratiti na početak niza.
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
-const Review = () => {
-  return <h2>review component</h2>;
+const Review = ({ person, hendleNext, hendlePrev, handleRandom }) => {
+  console.log(person);
+  const { image, name, job, text } = person;
+  return (
+    <article>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4>{name}</h4>
+      <p className='job-paraf margin-buttom'>{job}</p>
+      <p className='margin-buttom info'>{text}</p>
+      <div className='margin-buttom'>
+        <button className='chevron-icon prev-btn' onClick={hendlePrev}>
+          <FaChevronLeft />
+        </button>
+        <button className='chevron-icon next-btn' onClick={hendleNext}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='surprise-btn margin-buttom' onClick={handleRandom}>
+        Surprise me
+      </button>
+    </article>
+  );
 };
 
 export default Review;
