62b694bd01bd46d6fb72b0e744eaefdd8abd3723
aki024 <aleksa.simicc7@gmail.com>
2024-01-04 16:00:48 +0100

    add: footer mobile styling

--- 001_pripreme_css/Escoffier-full-site/script/script.js
+++ 001_pripreme_css/Escoffier-full-site/script/script.js
@@ -1,10 +1,14 @@
 if ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
 	const header = document.querySelector('header');
+	const stickyQuiz = document.querySelector('.stickyQuiz');
+
 	let observer = new IntersectionObserver((entries) => {
 		if (entries[0].boundingClientRect.y < 0) {
 			header.classList.add('headerScroll');
+			stickyQuiz.classList.add('active');
 		} else {
 			header.classList.remove('headerScroll');
+			stickyQuiz.classList.remove('active');
 		}
 	});
 	observer.observe(document.querySelector('#top-of-site-pixel-anchor'));
