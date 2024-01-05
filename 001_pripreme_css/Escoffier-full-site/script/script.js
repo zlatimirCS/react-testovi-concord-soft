a7322fede9a86eaaee73d6c0abf093f746f64f8e
aki024 <aleksa.simicc7@gmail.com>
2024-01-05 16:00:21 +0100

    add: responsive for all versions

--- 001_pripreme_css/Escoffier-full-site/script/script.js
+++ 001_pripreme_css/Escoffier-full-site/script/script.js
@@ -1,15 +1,27 @@
-if ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
-	const header = document.querySelector('header');
-	const stickyQuiz = document.querySelector('.stickyQuiz');
+const handleResize = () => {
+	let windowWidth = window.innerWidth;
 
-	let observer = new IntersectionObserver((entries) => {
-		if (entries[0].boundingClientRect.y < 0) {
-			header.classList.add('headerScroll');
-			stickyQuiz.classList.add('active');
-		} else {
-			header.classList.remove('headerScroll');
-			stickyQuiz.classList.remove('active');
-		}
-	});
-	observer.observe(document.querySelector('#top-of-site-pixel-anchor'));
-}
+	if ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+		const header = document.querySelector('header');
+		const stickyQuiz = document.querySelector('.stickyQuiz');
+
+		let observer = new IntersectionObserver((entries) => {
+			if (windowWidth > 1024) {
+				// return;
+			}
+			if (entries[0].boundingClientRect.y < 0) {
+				header.classList.add('headerScroll');
+				stickyQuiz.classList.add('active');
+			} else {
+				header.classList.remove('headerScroll');
+				stickyQuiz.classList.remove('active');
+			}
+		});
+		observer.observe(document.querySelector('#top-of-site-pixel-anchor'));
+	}
+};
+// Get the initial width
+handleResize();
+
+// Add the event listener for window resize
+window.addEventListener('resize', handleResize);
