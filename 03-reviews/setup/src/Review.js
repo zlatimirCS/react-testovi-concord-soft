6c48905f29a74cd39831f398294211566a62fe05
aki024 <aleksa.simicc7@gmail.com>
2024-01-09 12:21:54 +0100

    add: 03 vezba

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,62 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+	const [review, setReview] = useState(0);
+	const { name, job, image, text } = people[review];
+
+	// Review change handler
+	const handleReviewChange = (type) => {
+		const arrayLength = people.length;
+
+		if (type === undefined) {
+			setReview((prevState) => getRandomUniqueReview(prevState));
+			return;
+		}
+
+		if (type === 'next') {
+			setReview((prevState) => (prevState === arrayLength - 1 ? 0 : prevState + 1));
+		} else {
+			setReview((prevState) => (prevState === 0 ? arrayLength - 1 : prevState - 1));
+		}
+	};
+
+	// Get a random review that is not the current review
+	const getRandomUniqueReview = (currentReview) => {
+		const arrayLength = people.length;
+		let randomReview = Math.floor(Math.random() * arrayLength);
+
+		if (randomReview === currentReview) {
+			return getRandomUniqueReview(currentReview);
+		}
+		return randomReview;
+	};
+
+	return (
+		<article className='review'>
+			<div className='imageContainer'>
+				<img src={image} alt={name} className='personImg' />
+				<span className='quoteIcon'>
+					<FaQuoteRight />
+				</span>
+			</div>
+			<div className='textContainer'>
+				<h4 className='author'>{name}</h4>
+				<p className='job'>{job}</p>
+				<p className='info'>{text}</p>
+				<div className='buttonContainer'>
+					<button className='prevBtn' onClick={() => handleReviewChange('prev')}>
+						<FaChevronLeft />
+					</button>
+					<button className='nextBtn' onClick={() => handleReviewChange('next')}>
+						<FaChevronRight />
+					</button>
+				</div>
+				<button className='randomBtn' onClick={() => handleReviewChange()}>
+					surprise me
+				</button>
+			</div>
+		</article>
+	);
 };
 
 export default Review;
