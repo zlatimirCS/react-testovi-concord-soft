79c708906753cb12df37dd62223798db86d0ca92
aki024 <aleksa.simicc7@gmail.com>
2024-01-09 13:32:00 +0100

    add: 04-accordion

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 import data from './data';
-import SingleQuestion from './Question';
+import Question from './components/Question';
 function App() {
-  return <h2 className="test">accordion project setup</h2>;
+	const [questions, setQuestions] = useState(data);
+
+	return (
+		<main>
+			<div className='container'>
+				<h3>questions and answers about login</h3>
+				<section className='info'>
+					{questions.map((question) => {
+						return <Question key={question.id} {...question} />;
+					})}
+				</section>
+			</div>
+		</main>
+	);
 }
 
 export default App;
